7c0eb3c9ce7c2c92547a78d954bcb234153fc54c
FrancoYudica <francoyudica@gmail.com>
2024-12-21 22:54:22 -0300

    Save dialog with scale resolution factor

---
+++ code/genart/UI/generation/save_generated_image_dialog/save_generated_image_dialog.gd
@@ -0,0 +1,77 @@
+extends Control
+
+
+@export var close_button: Button
+@export var save_button: Button
+@export var scale_spin_box: SpinBox
+@export var resolution_label: Label
+@export var final_resolution_label: Label
+@export var file_dialog: FileDialog
+@export var save_texture: TextureRect
+
+var _src_img_generation_details: ImageGenerationDetails
+
+func _ready() -> void:
+	close_button.pressed.connect(
+		func():
+			visible = false
+	)
+	
+	save_button.pressed.connect(_save)
+	file_dialog.file_selected.connect(_on_file_dialog_file_selected)
+	scale_spin_box.value_changed.connect(
+		func(value):
+			final_resolution_label.text = "%sx%s" % [
+				int(_src_img_generation_details.viewport_size.x * scale_spin_box.value),
+				int(_src_img_generation_details.viewport_size.y * scale_spin_box.value)]
+	)
+
+func open(gen_details: ImageGenerationDetails):
+	visible = true
+	_src_img_generation_details = gen_details
+	
+	resolution_label.text = "%sx%s" % [
+		_src_img_generation_details.viewport_size.x,
+		_src_img_generation_details.viewport_size.y]
+	
+	final_resolution_label.text = "%sx%s" % [
+		int(_src_img_generation_details.viewport_size.x * scale_spin_box.value),
+		int(_src_img_generation_details.viewport_size.y * scale_spin_box.value)]
+	
+	save_texture.texture = RenderingCommon.create_texture_from_rd_rid(
+		_src_img_generation_details.generated_texture.rd_rid)
+
+func _save():
+	file_dialog.visible = true
+
+func _on_file_dialog_file_selected(path: String) -> void:
+	
+	# Creates scaled individuals and ImageGenerationDetails with new data
+	var render_details := ImageGenerationDetails.new()
+	render_details.clear_color = _src_img_generation_details.clear_color
+	render_details.viewport_size = Vector2(
+		_src_img_generation_details.viewport_size.x * scale_spin_box.value,
+		_src_img_generation_details.viewport_size.y * scale_spin_box.value
+	)
+	for individual in _src_img_generation_details.individuals:
+		var ind = individual.copy()
+		ind.position *= scale_spin_box.value
+		ind.size *= scale_spin_box.value
+		render_details.individuals.append(ind)
+	
+	# Renders the individuals
+	ImageGenerationRenderer.render_image_generation(Renderer, render_details)
+	
+	# Gets renderer output texture
+	var color_attachment_texture = Renderer.get_attachment_texture(Renderer.FramebufferAttachment.COLOR)
+	var color_attachment_data = Renderer.rd.texture_get_data(color_attachment_texture.rd_rid, 0)
+	
+	# Transforms to image and saves
+	var img = ImageUtils.create_image_from_rgbaf_buffer(
+		render_details.viewport_size.x,
+		render_details.viewport_size.y,
+		color_attachment_data
+	)
+	
+	if img.save_png(path) == OK:
+		Notifier.notify_info("Successfully saved image at: %s" % path)
--- code/genart/UI/generation/save_output_button.gd
+++ code/genart/UI/generation/save_output_button.gd
@@ -2,7 +2,7 @@ extends TextureButton
 
 @export var image_generation: Node
 @export var output_texture_rect: TextureRect
-@export var file_dialog: FileDialog
+@export var save_dialog: Control
 
 func _ready() -> void:
 	
@@ -22,20 +22,4 @@ func _ready() -> void:
 
 
 func _on_pressed() -> void:
-	file_dialog.visible = true
-
-func _on_file_dialog_file_selected(path: String) -> void:
-		# Gets color attachment data
-		var color_attachment_texture = Renderer.get_attachment_texture(Renderer.FramebufferAttachment.COLOR)
-		var color_attachment_data = Renderer.rd.texture_get_data(color_attachment_texture.rd_rid, 0)
-		
-		var img = ImageUtils.create_image_from_rgbaf_buffer(
-			output_texture_rect.texture.get_width(),
-			output_texture_rect.texture.get_height(),
-			color_attachment_data
-		)
-		
-		if img.save_png(path) == OK:
-			Notifier.notify_info("Successfully saved image at: %s" % path)
-		else:
-			Notifier.notify_info("Error (%s) while saving image at: %s" % path)
+	save_dialog.open(image_generation.image_generation_details)
