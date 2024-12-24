7e035b4290522ac37093014baab954ade1c7fb14
FrancoYudica <francoyudica@gmail.com>
2024-12-24 16:03:57 -0300

    Clear color strategy uses parallel reduction

--- code/genart/generation/image_generation/clear_color/strategies/clear_color_average_strategy.gd
+++ code/genart/generation/image_generation/clear_color/strategies/clear_color_average_strategy.gd
@@ -3,7 +3,7 @@ extends ClearColorStrategy
 var _color_sampler: AverageColorSampler
 
 func _init() -> void:
-	_color_sampler = load("res://generation/average_color_sampler/avg_subrect/average_color_subrect_sampler_compute.gd").new()
+	_color_sampler = load("res://generation/average_color_sampler/avg_subrect/average_color_subrect_sampler_compute_reduction.gd").new()
 
 func get_clear_color() -> Color:
 	var rect = Rect2i(0, 0, sample_texture.get_width(), sample_texture.get_height())
