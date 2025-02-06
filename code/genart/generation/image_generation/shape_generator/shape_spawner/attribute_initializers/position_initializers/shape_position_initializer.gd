9f225e0b70c79ec7420e24169ecc736da30967e0
FrancoYudica <francoyudica@gmail.com>
2025-02-06 10:52:18 -0300

    Removed custom weight generator shape position initializer

--- code/genart/generation/image_generation/shape_generator/shape_spawner/attribute_initializers/position_initializers/shape_position_initializer.gd
+++ code/genart/generation/image_generation/shape_generator/shape_spawner/attribute_initializers/position_initializers/shape_position_initializer.gd
@@ -3,8 +3,7 @@ class_name ShapePositionInitializer extends ShapeAttributeInitializer
 enum Type
 {
 	RANDOM,
-	USE_WEIGHT_TEXTURE,
-	CUSTOM_WEIGHT_TEXTURE
+	USE_WEIGHT_TEXTURE
 }
 
 static func factory_create(type: Type) -> ShapePositionInitializer:
@@ -13,8 +12,6 @@ static func factory_create(type: Type) -> ShapePositionInitializer:
 			return load("res://generation/image_generation/shape_generator/shape_spawner/attribute_initializers/position_initializers/shape_random_position_initializer.gd").new()
 		Type.USE_WEIGHT_TEXTURE:
 			return load("res://generation/image_generation/shape_generator/shape_spawner/attribute_initializers/position_initializers/shape_use_weight_position_initializer.gd").new()
-		Type.CUSTOM_WEIGHT_TEXTURE:
-			return load("res://generation/image_generation/shape_generator/shape_spawner/attribute_initializers/position_initializers/shape_custom_weight_position_initializer.gd").new()
 		_:
 			push_error("Unimplemented ShapePositionInitializer of type %s" % type)
 			return null
