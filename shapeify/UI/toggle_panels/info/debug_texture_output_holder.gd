b6364b8c16ce71514f51137149eae27da20db26e
FrancoYudica <francoyudica@gmail.com>
2025-02-26 09:27:43 -0300

    Removed Texture2DRD frees. Replaced by AutoFreeTexture2DRD

--- shapeify/UI/toggle_panels/info/debug_texture_output_holder.gd
+++ shapeify/UI/toggle_panels/info/debug_texture_output_holder.gd
@@ -10,9 +10,6 @@ var _mutex := Mutex.new()
 func _ready() -> void:
 	DebugSignals.connect(debug_signal_name, _update_texture_ref)
 
-func _exit_tree() -> void:
-	_free()
-
 func _update_texture_ref(updated_texture: LocalTexture):
 	_mutex.lock()
 	_updated_texture = updated_texture
@@ -26,16 +23,8 @@ func update_texture_rect(texture_rect: TextureRect):
 	
 	# Uses mutex since _update_texture_ref might gets called in another thread
 	_mutex.lock()
-	_free()
 	_texture_rd = _updated_texture.create_texture_2d_rd()
 	texture_rect.texture = _texture_rd
 	_updated_texture = null
 	_mutex.unlock()
 	
-
-func _free():
-	if _texture_rd == null or not _texture_rd.texture_rd_rid.is_valid():
-		return
-	
-	var rd = RenderingServer.get_rendering_device()
-	rd.free_rid(_texture_rd.texture_rd_rid)
--- shapeify/tests/average_color/subrect/average_color_test.gd
+++ shapeify/tests/average_color/subrect/average_color_test.gd
@@ -12,26 +12,21 @@ var compute_sampler: AverageColorSampler
 @onready var sample_color_rect := $OutlineColorRect/SampleColorRect
 @onready var parent_color_rect := $OutlineColorRect
 
+var _sample_texure: LocalTexture
 
 func _ready() -> void:
 	
 	cpu_sampler = cpu_sampler_script.new()
 	compute_sampler = compute_sampler_script.new()
 	average_color_sampler = compute_sampler
-	RenderingServer.frame_post_draw.connect(_frame_post_draw)
+
 	
-	
-func _frame_post_draw() -> void:
-	
-	# Get the subviewport texture for the first time
-	if average_color_sampler.sample_texture == null or not average_color_sampler.sample_texture.is_valid():
-		var texture = sub_viewport.get_texture()
-		var sample_texture_rd_rid = RenderingCommon.create_local_rd_texture_copy(texture)
-		var sample_texture = LocalTexture.new()
-		sample_texture.rd_rid = sample_texture_rd_rid
-		average_color_sampler.sample_texture = sample_texture
-		cpu_sampler.sample_texture = sample_texture
-		compute_sampler.sample_texture = sample_texture
+func _process(delta: float) -> void:
+	var texture = sub_viewport.get_texture()
+	_sample_texure = LocalTexture.load_from_texture(texture, GenerationGlobals.renderer.rd)
+	average_color_sampler.sample_texture = _sample_texure
+	cpu_sampler.sample_texture = _sample_texure
+	compute_sampler.sample_texture = _sample_texure
 	
 	var rect: Rect2i
 	var mouse = get_local_mouse_position()
